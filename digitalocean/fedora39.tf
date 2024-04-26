bd3c8862be18a039b9ee564e2607a77c9b64ad97
straysheep-dev <bah@straysheep.dev>
2024-04-25 23:42:07 -0700

    Add digitalocean/fedora39.tf

---
+++ digitalocean/fedora39.tf
@@ -0,0 +1,31 @@
+resource "digitalocean_droplet" "terraform-fedora-39-x64" {
+  image = "fedora-39-x64"
+  name = "terraform-fedora-39-x64"
+  region = "sfo3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    # Use `private_key` if you're using a private key file
+    #private_key = file(var.pvt_key)
+    # Use `agent = true` if your ssh key is on a Yubikey and the agent can read it
+    # Be patient while it's "Still creating...", it can take a minute or two before the Yubikey is called
+    agent = true
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf upgrade -yq",
+      "sudo dnf install -yq python3-pip git tmux",
+      "python3 -m pip install --user ansible",
+      "gpg --keyid-format long --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys '9906 9EB1 2D40 9EA9 3BD1  E52E B09D 00AE C481 71E0'",
+      "cd ~/; git clone https://github.com/straysheep-dev/ansible-configs.git",
+      "cd ~/; git clone https://github.com/straysheep-dev/linux-configs.git",
+      "sudo systemctl reboot"
+    ]
+  }
+}
