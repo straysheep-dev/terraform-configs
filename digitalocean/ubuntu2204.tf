1af0385ee20182f3abc5f5f9da8cb4700646f4fa
straysheep-dev <bah@straysheep.dev>
2024-04-25 23:39:07 -0700

    Update digitalocean/ubuntu2204.tf

--- digitalocean/ubuntu2204.tf
+++ digitalocean/ubuntu2204.tf
@@ -1,6 +1,6 @@
-resource "digitalocean_droplet" "terraform-test" {
+resource "digitalocean_droplet" "terraform-ubuntu-22-04-x64" {
   image = "ubuntu-22-04-x64"
-  name = "terraform-test"
+  name = "terraform-ubuntu-22-04-x64"
   region = "sfo3"
   size = "s-1vcpu-1gb"
   ssh_keys = [
@@ -23,13 +23,15 @@ resource "digitalocean_droplet" "terraform-test" {
       "export DEBIAN_FRONTEND=noninteractive",
       # Update the package cache, enable UFW, install ansible, add a public GPG key, clone git repos
       "sudo apt-get update -yq",
+      "sudo apt-get upgrade -yq",
       "sudo ufw allow ssh",
       "echo 'y' | sudo ufw enable",
       "apt install -yq python3-pip",
       "python3 -m pip install --user ansible",
       "gpg --keyid-format long --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys '9906 9EB1 2D40 9EA9 3BD1  E52E B09D 00AE C481 71E0'",
       "cd ~/; git clone https://github.com/straysheep-dev/ansible-configs.git",
-      "cd ~/; git clone https://github.com/straysheep-dev/linux-configs.git"
+      "cd ~/; git clone https://github.com/straysheep-dev/linux-configs.git",
+      "sudo systemctl reboot"
     ]
   }
 }
