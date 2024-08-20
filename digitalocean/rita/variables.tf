3c6b78abd5e72aaaa508a54e66288fcd00ad370c
straysheep-dev <bah@straysheep.dev>
2024-08-20 05:48:27 -0700

    Fix rita/variables.tf

--- digitalocean/rita/variables.tf
+++ digitalocean/rita/variables.tf
@@ -1,6 +1,5 @@
 variable "do_token" {}
-# "pvt_key" is uncommented to use a private key file instead
-# of a Yubikey.
-variable "pvt_key" {}
+# Uncomment "pvt_key" if using a passwordless private key file
+#variable "pvt_key" {}
 variable "key_name" {}
 variable "region_choice" {}
